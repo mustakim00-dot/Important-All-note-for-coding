11aef9f5a1ccafbca30a814a89370ad80fab67a9
Md .Mustakim Billah <mdmustakimkhan279@gmail.com>
2026-01-13 03:02:50 +0600

    npm install
    Installed axios, react-toastify, react-query, react-form, and zustand.

--- apiConnect.js
+++ apiConnect.js
@@ -6,3 +6,8 @@ export const api =  axios.create({
     headers: { accept: "application/json", "content-type": "application/json" },
     // timeout: 2000,
 });
+npm i axios
+npm i react-toastify
+npm i @tanstack/react-query
+npm i @tanstack/react-form
+npm i zustand
