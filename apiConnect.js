608cc13386f6c89c94c544888fd3f7aeb2c064af
Md .Mustakim Billah <mdmustakimkhan279@gmail.com>
2026-01-13 03:38:35 +0600

    Implement CORS with dynamic origin whitelist
    Add CORS middleware and update whitelist origins

--- apiConnect.js
+++ apiConnect.js
@@ -11,3 +11,22 @@ npm i react-toastify
 npm i @tanstack/react-query
 npm i @tanstack/react-form
 npm i zustand
+npm i tailwind-merge
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || WHITELIST.includes(origin)) {
+        callback(null, origin);
+        //callback()
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // যদি cookie / auth দরকার হয়
+  })
+);
+
+PORT = 8000
+const WHITELIST = process.env.WHITELIST || ["http://localhost:9000/"];
+WHITELIST=["http://localhost:9000/","http://localhost:5173/"];
